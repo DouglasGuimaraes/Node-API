bf007fa36c1a893c3bbadec404c61e7942417d04
Douglas Guimarães <dmguimaraes@stefanini.com>
2020-06-08 18:50:02 -0300

    Adjusts.

--- api/controllers/customerWallets.js
+++ api/controllers/customerWallets.js
@@ -6,26 +6,24 @@ module.exports = app => {
   
     const {
       customerWallets: customerWalletsMock
-    } = customerWalletsDB
+    } = customerWalletsDB;
 
     controller.listCustomerWallets = (req, res) => res.status(200).json(customerWalletsDB);
 
     controller.saveCustomerWallets = (req, res) => {
-      customerWalletsMock.data.push(
-        {
+      customerWalletsMock.data.push({
           id: uuidv4(),
           parentId: uuidv4(),
-          name = req.body.name,
+          name: req.body.name,
           birthDate: req.body.birthDate,
           cellphone: req.body.cellphone,
           phone: req.body.phone,
           email: req.body.email,
           occupation: req.body.occupation,
           state: req.body.state
-        }
-      );
+        });
       res.status(201).json(customerWalletsMock);
-    };
+    }
   
     return controller;
   }
